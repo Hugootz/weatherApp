dad59bdc70e1d328cc0a8d1307df5805dc85394f
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-08 13:29:21 -0300

    estilização dos componentes

--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -1,4 +1,19 @@
-import styled from "styled-components/native";
+import { styled } from "styled-components/native";
+import { TextInput, ImageBackground } from "react-native";
 
-export const Container = styled.View``;
-export const Title = styled.Text``;
+export const Container = styled.View`
+  flex: 1;
+`;
+export const Header = styled.View``;
+
+export const Bg = styled(ImageBackground)`
+  flex: 1;
+  width: 420px;
+  height: 860px;
+  align-items: center;
+`;
+
+export const Input = styled(TextInput)`
+  top: 60px;
+  color: #fff;
+`;
