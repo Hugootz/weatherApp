9c693c3f28cc400d3ce793c04ca78b3fc54b4e61
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-12 16:22:17 -0300

    page Home

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,51 +1,10 @@
-import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  Container,
-  Bg,
-  Input,
-  Header,
-  InputButton,
-  IconButton,
-  Locations,
-  Press,
-  Title,
-} from "./styles";
-import { DismissKeyboard } from "../components/DismissKeyboard";
+import React from "react";
+import { Container, Title } from "./styles";
 
 export function Home() {
-  const [showSearch, setShowSearch] = useState(false);
-  const [locations, setLocations] = useState([1, 2, 3]);
   return (
-    <DismissKeyboard>
-      <Container>
-        <StatusBar style="light" />
-        <Bg blurRadius={50} source={require("../assets/images/wp5198875.jpg")}>
-          <Header>
-            {showSearch ? (
-              <Input
-                placeholder="Find the location..."
-                placeholderTextColor={"#cecece"}
-              />
-            ) : null}
-            <InputButton onPress={() => setShowSearch(!showSearch)}>
-              <IconButton name="search1" size={24} color="#fff" />
-            </InputButton>
-
-            {locations.length > 0 && showSearch ? (
-              <Locations>
-                {locations.map((loc, index) => {
-                  return (
-                    <Press key={index}>
-                      <Title>London, United Kingdom</Title>
-                    </Press>
-                  );
-                })}
-              </Locations>
-            ) : null}
-          </Header>
-        </Bg>
-      </Container>
-    </DismissKeyboard>
+    <Container>
+      <Title>Welcome Home</Title>
+    </Container>
   );
 }
