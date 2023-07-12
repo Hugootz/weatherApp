4aef91b2380108c2bf9c38c505c9bf2e40d4d5c8
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-12 16:21:41 -0300

    styled components

--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -1,41 +1,8 @@
 import { styled } from "styled-components/native";
-import { TextInput, ImageBackground } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+
 export const Container = styled.View`
   flex: 1;
-`;
-export const Header = styled.View``;
-
-export const Bg = styled(ImageBackground)`
-  flex: 1;
-  width: 420px;
-  height: 860px;
+  justify-content: center;
   align-items: center;
 `;
-
-export const Input = styled(TextInput)`
-  top: 60px;
-  color: #cecece;
-  background-color: rgba(0, 0, 0, 0.3);
-  width: 350px;
-  height: 45px;
-  align-items: center;
-  border-radius: 15px;
-  padding: 12px;
-`;
-export const InputButton = styled.TouchableOpacity`
-  align-self: flex-end;
-  top: 65px;
-  left: 140px;
-`;
-export const IconButton = styled(AntDesign)``;
-export const Locations = styled.View``;
-export const Press = styled.TouchableOpacity``;
-export const Title = styled.Text`
-  color: #fff;
-  top: 42px;
-  background-color: rgba(0, 0, 0, 0.3);
-  border-radius: 15px;
-  height: 50px;
-  padding: 12px;
-`;
+export const Title = styled.Text``;
