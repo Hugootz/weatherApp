c2bc6e0c90b05525297cc5fc2bcfdd644bcf2670
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-13 10:18:11 -0300

    adição do estado

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Title } from "./styles";
 
 export function Home() {
+  const [darktheme, setDarkTheme] = useState(true);
+  const [currentTemperature, setCurrentTemperature] = useState("27");
+  const [location, setLocation] = useState("BR,Fortaleza");
   return (
     <Container>
       <Title>Welcome Home</Title>
