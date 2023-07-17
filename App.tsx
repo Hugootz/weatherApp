547b81a660f9847516f9737e38ed5deb6f34b896
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-17 17:02:05 -0300

    add theme provider

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Home } from "./src/Home";
-
+import theme from "./src/global/styles/theme";
+import { ThemeProvider } from "styled-components";
 export default function App() {
-  return <Home />;
+  return;
+  <ThemeProvider theme={theme}>
+    <Home />;
+  </ThemeProvider>;
 }
