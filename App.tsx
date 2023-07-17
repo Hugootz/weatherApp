5a68ecf924ff3303885c8a508e417ba9cf0a3313
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-17 19:11:42 -0300

    correção da pasta app

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import { Home } from "./src/Home";
 import theme from "./src/global/styles/theme";
 import { ThemeProvider } from "styled-components";
 export default function App() {
-  return;
-  <ThemeProvider theme={theme}>
-    <Home />;
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
