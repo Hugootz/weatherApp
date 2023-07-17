56bfc73f830b06aa85aa3bfc35a2827c6468a8cb
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-17 19:10:38 -0300

    estilização dos componentes e ternários p/ temas

--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -1,8 +1,39 @@
 import { styled } from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { EvilIcons } from "@expo/vector-icons";
+import theme from "../global/styles/theme";
 
-export const Container = styled.View`
+interface StyledProps {
+  isActive: Boolean;
+}
+
+export const Container = styled.View<StyledProps>`
   flex: 1;
-  justify-content: center;
   align-items: center;
+  background-color: ${({ isActive }) =>
+    isActive ? theme.colors.background : theme.colors.background_light};
 `;
-export const Title = styled.Text``;
+export const Refresh = styled(EvilIcons)`
+  font-size: ${RFValue(35)}px;
+  color: #fff;
+`;
+export const IconSun = styled(Feather)`
+  color: #ffe338;
+  margin-top: 50px;
+  font-size: ${RFValue(40)}px;
+`;
+export const Header = styled.View`
+  flex-direction: row;
+  margin-top: 10px;
+`;
+export const Temperature = styled.Text<StyledProps>`
+  font-size: ${RFValue(45)}px;
+  color: ${({ isActive }) =>
+    isActive ? theme.colors.title : theme.colors.title_dark};
+`;
+export const Symbol = styled.Text<StyledProps>`
+  font-size: ${RFValue(18)}px;
+  color: ${({ isActive }) =>
+    isActive ? theme.colors.title : theme.colors.title_dark};
+`;
