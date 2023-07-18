d4ab9f2b8b18ebcc8165655e5a7b8a3b12551501
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-18 16:06:29 -0300

    adição da statusBar

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/Home";
 import theme from "./src/global/styles/theme";
 import { ThemeProvider } from "styled-components";
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" />
       <Home />
     </ThemeProvider>
   );
