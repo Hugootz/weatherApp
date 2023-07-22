ebb45a666166832e6e57248341b7cf3e90962ec7
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-22 14:47:57 -0300

    estilização do componente MainCard

--- src/components/MainCard/styles.ts
+++ src/components/MainCard/styles.ts
@@ -1,9 +1,8 @@
-import { Feather } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
 import { RFValue } from "react-native-responsive-fontsize";
 import { styled } from "styled-components/native";
 
 export const Container = styled.View`
-  background-color: red;
   justify-content: center;
   align-items: center;
   margin: 15px;
@@ -11,9 +10,15 @@ export const Container = styled.View`
   height: 210px;
   border-radius: 15px;
 `;
-export const Title = styled.Text``;
-export const IconMorning = styled(Feather)`
-  color: ${({ theme }) => theme.colors.sun};
-  font-size: ${RFValue(40)}px;
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.background_light};
+  font-size: ${RFValue(18)}px;
 `;
-export const TemperatureCard = styled.Text``;
+export const IconCard = styled(Fontisto)`
+  color: ${({ theme }) => theme.colors.background_light};
+  font-size: ${RFValue(40)}px;
+  margin: 20px;
+`;
+export const TemperatureCard = styled.Text`
+  color: ${({ theme }) => theme.colors.background_light};
+`;
