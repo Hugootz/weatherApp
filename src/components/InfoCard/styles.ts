4710c06c35a1a0f6638d80bef52009df3c2f0e43
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-07-24 15:49:02 -0300

    estilização InfoCard

--- src/components/InfoCard/styles.ts
+++ src/components/InfoCard/styles.ts
@@ -4,8 +4,11 @@ import { RFValue } from "react-native-responsive-fontsize";
 export const Container = styled.View`
   flex: 1;
   align-items: center;
-  margin: 10px;
+  margin: 5px;
   min-width: 150px;
+  border-width: 3px;
+  border-color: ${({ theme }) => theme.colors.text_card2};
+  border-radius: 10px;
 `;
 export const Title = styled.Text`
   color: ${({ theme }) => theme.colors.text_card};
@@ -13,13 +16,13 @@ export const Title = styled.Text`
   margin-left: 15px;
   font-size: ${RFValue(17)}px;
   font-weight: bold;
-  bottom: 10px;
+  bottom: 5px;
   align-self: center;
 `;
 
 export const Value = styled.Text`
   color: ${({ theme }) => theme.colors.text_card2};
-  bottom: 10px;
+  bottom: 8px;
   margin-left: 15px;
   font-size: ${RFValue(15)}px;
   align-self: center;
